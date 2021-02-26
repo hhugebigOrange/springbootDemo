597f6b2c3d7be9bf0aa185f83b297168ce3f4e7d
tao.wu <381282043@qq.com>
2021-02-26 16:01:38 +0800

    springboot集成swagger2

--- src/main/java/com/wt/demo/controller/BaseController.java
+++ src/main/java/com/wt/demo/controller/BaseController.java
@@ -2,18 +2,22 @@ package com.wt.demo.controller;
 
 import com.wt.demo.entity.Test;
 import com.wt.demo.service.TestService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@Api("BaseController相关的api")
 public class BaseController {
 
     @Autowired
     private TestService testService;
 
-    @RequestMapping("/getTest")
+    @PostMapping("/getTest")
+    @ApiOperation(value = "测试", notes = "测试用")
     public List<Test> getTest() {
         return testService.getTest();
     }
