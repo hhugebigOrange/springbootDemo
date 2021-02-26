aa31e8928be0947faad6d433a95fd61c184b4c75
tao.wu <381282043@qq.com>
2021-02-26 09:49:23 +0800

    springboot集成log4j2

--- src/main/java/com/wt/demo/DemoApplication.java
+++ src/main/java/com/wt/demo/DemoApplication.java
@@ -1,12 +1,19 @@
 package com.wt.demo;
 
+
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class DemoApplication {
 
+
     public static void main(String[] args) {
+        log.info("123");
+        log.warn("123");
+        log.error("123");
         SpringApplication.run(DemoApplication.class, args);
     }
 
